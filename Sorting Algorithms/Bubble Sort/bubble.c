33ed6794727820d4b4291ee40485b91120870052
Prince Isaac Pantino <arispantino@gmail.com>
2024-09-22 18:04:17 +0800

    feat: added base bubble sort implementation

---
+++ Sorting Algorithms/Bubble Sort/bubble.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void bubbleSort(int *arr, int size);
+
+int main(){
+
+    int size = 10;
+    int arr[] = {8, 6, 4, 9, 0, 1, 2, 6, 3, 9};
+
+    bubbleSort(arr, size);
+    int i;
+    for(i = 0; i < size; i++){
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
+
+void bubbleSort(int *arr, int size){
+
+    int i, j;
+    int swapped;
+    for(i = 0; i < size - 1 ; i++){
+        swapped = 0;
+        for(j = 0; j < size - i - 1; j++){
+            if(arr[j] > arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                swapped = 1;
+            }
+        }
+    }
+
+}
