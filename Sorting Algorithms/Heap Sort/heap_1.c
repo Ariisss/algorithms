1f8007677752f428806db1caf9da16bee9aebcfb
Prince Isaac Pantino <arispantino@gmail.com>
2024-09-23 16:15:50 +0800

    docs(heap_1.c): added documentation

--- Sorting Algorithms/Heap Sort/heap_1.c
+++ Sorting Algorithms/Heap Sort/heap_1.c
@@ -15,20 +15,29 @@ int main()
     printArray(arr, N);
 }
 
+// max is the max size of the array and i is the current parent
 void heapify(int *arr, int max, int i){
 
+    // high holds the index of the largest element since this is a max heap
     int high = i;
+    
+    // index of left child
     int leftChild = i * 2 + 1;
+
+    //index of right child
     int rightChild = i * 2 + 2;
 
+    // if left child is greater than parent, high index becomes the left child index
     if(leftChild < max && arr[leftChild] > arr[high]){
         high = leftChild;
     }
 
+    // the same as left child logic
     if(rightChild < max && arr[rightChild] > arr[high]){
         high = rightChild;
     }
 
+    // if the high index changed (a child is greater than the initial root/parent node), we swap both then heapify with the current high as the parent/root node.
     if(high != i){
         swap(&arr[i], &arr[high]);
         heapify(arr, max, high);
@@ -39,10 +48,12 @@ void heapify(int *arr, int max, int i){
 void heapSort(int *arr, int max){
 
     int i, j;
+    // building a max heap tree starting from the lowest level parent
     for(i = max/2 - 1; i >= 0; i--){
         heapify(arr, max, i);
     }
 
+    // delete first and heapifying starting from the root node (index 0)
     for(i = max - 1; i >= 0; i--){
         swap(&arr[i], &arr[0]);
         heapify(arr, i, 0);
