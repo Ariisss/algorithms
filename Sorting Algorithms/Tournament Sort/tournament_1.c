bf15b4eb0cfc0d0887ecd39fb950f0fd56da6339
Prince Isaac Pantino <arispantino@gmail.com>
2024-09-23 21:08:43 +0800

    feat(tournament_1.c): added helper functions

--- Sorting Algorithms/Tournament Sort/tournament_1.c
+++ Sorting Algorithms/Tournament Sort/tournament_1.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
 
+void printArray(int arr[], int N);
+void swap(int *a, int *b);
 
 int main(){
 
 
 
     return 0;
+}
+
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void printArray(int arr[], int N)
+{
+    for (int i = 0; i < N; i++){
+        printf("%d ", arr[i]);
+    }
+
 }
