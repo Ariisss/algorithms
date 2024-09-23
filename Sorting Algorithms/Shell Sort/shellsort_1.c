37ebae38dae3532be4464e879a3c9791b8e0fe70
Prince Isaac Pantino <arispantino@gmail.com>
2024-09-23 14:21:57 +0800

    feat: implemented base shell sort algorithm

--- Sorting Algorithms/Shell Sort/shellsort_1.c
+++ Sorting Algorithms/Shell Sort/shellsort_1.c
@@ -2,12 +2,32 @@
 
 void printArray(int arr[], int N);
 void swap(int *a, int *b);
-void shellSort(int *arr, int N);
+void shellSort(int *arr, int size);
 
 int main(){
 
     int arr[] = { 12, 11, 13, 5, 6, 7, 2, 5, 15 , 1, 0 };
     int N = sizeof(arr) / sizeof(arr[0]);
+
+    shellSort(arr, N);
+    printArray(arr, N);
+
+}
+
+void shellSort(int *arr, int size){
+
+    int gap, i, j;
+
+    for(gap = size/2; gap > 0; gap /= 2){
+        for(i = gap; i < size; i++){
+
+            int key = arr[i];
+            for(j = i; j >= gap && key < arr[j-gap]; j -= gap){
+                arr[j] = arr[j - gap];
+            }
+            arr[j] = key;
+        }
+    }
 
 }
 
