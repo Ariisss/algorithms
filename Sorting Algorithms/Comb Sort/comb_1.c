08222e9b2bd69bfe97b198f19795dc33e792c4d0
Prince Isaac Pantino <arispantino@gmail.com>
2024-09-23 16:07:31 +0800

    feat(comb1_c): implemented base comb sort algorithm

--- Sorting Algorithms/Comb Sort/comb_1.c
+++ Sorting Algorithms/Comb Sort/comb_1.c
@@ -9,7 +9,39 @@ int main(){
     int arr[] = { 12, 11, 13, 5, 6, 7, 2, 5, 15 , 1, 0 };
     int N = sizeof(arr) / sizeof(arr[0]);
 
+    combSort(arr, N);
+    printArray(arr, N);
+
+
     return 0;
+}
+
+void combSort(int *arr, int size){
+
+    int gap, i;
+    int swapped = 1;
+
+    // loop until gap is not one OR if the swap flag is true to check for misplaced elements in the array
+    while(gap != 1 || swapped == 1){
+
+        // shrink gap by 1.3
+        gap /= 1.3;
+
+        //if gap is less than one, we turn gap to 1 to account for checking loops (when swapped = 1 and gap is already 1)
+        if(gap < 1){
+            gap = 1;
+        }
+        swapped = 0;
+
+        // this is basically bubble sort but comparing elements with a gap interval
+        for(i = 0; (i + gap) < size; i++){
+            if(arr[i] > arr[i + gap]){
+                swap(&arr[i], &arr[i + gap]);
+                swapped = 1;
+            }
+        }
+    }
+
 }
 
 void swap(int *a, int *b){
