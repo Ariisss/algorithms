eb8ef064fe3c00ed37714006ab134ebe3bb25141
Prince Isaac Pantino <arispantino@gmail.com>
2024-09-23 16:28:31 +0800

    docs(selection_1.c): added documentation

--- Sorting Algorithms/Selection Sort/selection_1.c
+++ Sorting Algorithms/Selection Sort/selection_1.c
@@ -20,15 +20,20 @@ void selectionSort(int *arr, int size){
 
     int i, j, min;
 
+    // loop until the end
     for(i = 0; i < size; i++){
 
+        // hold the index of i
         min = i;
+
+        // loop until the end and keep checking for an element lesser than arr[min]
         for(j = i + 1; j < size; j++){
             if(arr[j] < arr[min]){
                 min = j;
             }
         }
 
+        // if there's an element lesser than min, swap them to place it in it's proper position
         if(min != i){
             int temp = arr[min];
             arr[min] = arr[i];
