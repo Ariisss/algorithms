1a6b627bb3b54766222b7177d8eda3d9ab9f20de
Prince Isaac Pantino <arispantino@gmail.com>
2024-09-23 12:13:12 +0800

    feat: increased test array size

--- Sorting Algorithms/Heap Sort/heap_1.c
+++ Sorting Algorithms/Heap Sort/heap_1.c
@@ -7,7 +7,7 @@ void printArray(int arr[], int N);
 
 int main()
 {
-    int arr[] = { 12, 11, 13, 5, 6, 7 };
+    int arr[] = { 12, 11, 13, 5, 6, 7, 2, 5, 15 , 1, 0 };
     int N = sizeof(arr) / sizeof(arr[0]);
 
     heapSort(arr, N);
