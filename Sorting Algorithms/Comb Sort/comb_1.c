3cf718ecb61f353589ace8649bc2ce04a26ad97f
Prince Isaac Pantino <arispantino@gmail.com>
2024-09-23 14:23:57 +0800

    feat(comb_1.c): added helper functions

--- Sorting Algorithms/Comb Sort/comb_1.c
+++ Sorting Algorithms/Comb Sort/comb_1.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 
-
+void printArray(int arr[], int N);
+void swap(int *a, int *b);
+void combSort(int *arr, int size);
 
 int main(){
 
-
+    int arr[] = { 12, 11, 13, 5, 6, 7, 2, 5, 15 , 1, 0 };
+    int N = sizeof(arr) / sizeof(arr[0]);
 
     return 0;
 }
 
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void printArray(int arr[], int N)
+{
+    for (int i = 0; i < N; i++){
+        printf("%d ", arr[i]);
+    }
+
+}
