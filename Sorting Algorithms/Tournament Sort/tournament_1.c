e92db1fb34d27a7239e5b32f5dba0b2b97c13db5
Prince Isaac Pantino <arispantino@gmail.com>
2024-09-24 23:37:39 +0800

    feat(tournament_1.c): added partial tournament sort indexing logic (credits to ced hehe)

--- Sorting Algorithms/Tournament Sort/tournament_1.c
+++ Sorting Algorithms/Tournament Sort/tournament_1.c
@@ -1,13 +1,51 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void printArray(int arr[], int N);
 void swap(int *a, int *b);
+void tournaSort(int *arr, int size);
 
 int main(){
 
+    int arr[] = { 12, 11, 13, 5, 6, 7, 2, 5, 15 , 1, 0 };
+    int N = sizeof(arr) / sizeof(arr[0]);
 
+    // tournaSort(arr, N);
 
     return 0;
+}
+
+void tournaSort(int *arr, int size){
+
+    int heapSize = (size * 2 - 1);
+    int* heap = (int*)malloc(heapSize * sizeof(int));
+
+    int i, j, x = heapSize;
+    int winFlag = 0;
+
+    for(i = size - 1; i >= 0; i--){
+        heap[x - 1] = arr[i];
+        x--;
+    }
+
+    for(j = 0; j < size; j++){
+        for(i = (heapSize/2) - 1; i >= 0; i--){
+            
+            int LC = (i*2) + 1;
+            int RC = (i*2) + 2;
+            
+            int left = LC < heapSize - size ? heap[LC] : LC;
+            int right = RC < heapSize - size ? heap[RC] : RC;
+
+            heap[j] = heap[left] < heap[right] ? left : right;
+
+            j = (i > 0 && j != 0) ? (j-1)/2 : j-1;
+        }
+
+    }
+
+
+
 }
 
 void swap(int *a, int *b){
