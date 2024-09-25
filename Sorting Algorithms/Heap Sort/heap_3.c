e47b196cb94fd60a4ce5e5c8b79d9cafd10a1b01
Prince Isaac Pantino <arispantino@gmail.com>
2024-09-25 23:28:19 +0800

    feat(heap_3): implemented max heapsort version 3 (ascending)

--- Sorting Algorithms/Heap Sort/heap_3.c
+++ Sorting Algorithms/Heap Sort/heap_3.c
@@ -10,59 +10,79 @@ typedef struct
 
 void swap(int *a, int *b);
 void heapify(HEAP heap, int i);
-void heapSort(int *arr, int max);
+void heapSort(HEAP heap);
 
 int main()
 {
-    int array[] = {12, 11, 13, 5, 6, 7, 2, 5, 15, 1, 0};
+    int array[] = {12, 11, 13, 5, 6, 7, 2, 5};
     int N = sizeof(array) / sizeof(array[0]);
 
     HEAP heap = (HEAP)malloc(sizeof(HEAP));
 
     if (heap != NULL)
     {
-        heap->arr = (int *)malloc(sizeof(int) * (N+1));
+        heap->arr = (int *)malloc(sizeof(int) * (N + 1));
         heap->arr[0] = 0;
     }
 
     int i;
 
-    for(i = 0; i < N; i++){
-        heap->arr[i+1] = array[i];
+    for (i = 0; i < N; i++)
+    {
+        heap->arr[i + 1] = array[i];
         heap->arr[0]++;
     }
 
-    for(i = heap->arr[0] / 2; i > 0; i--){
+    heapSort(heap);
+
+    printf("\nsorted array: ");
+    for (i = 1; i <= heap->arr[0]; i++)
+    {
+        printf("%d ", heap->arr[i]);
+    }
+}
+
+void heapSort(HEAP heap)
+{
+
+    int i, ctr = 1;
+    for (i = heap->arr[0] / 2; i > 0; i--)
+    {
         heapify(heap, i);
     }
 
-    for(i = 1; i <= heap->arr[0]; i++){
-        printf("%d ", heap->arr[i]);
+    int lastNdx = heap->arr[0];
+    for (i = heap->arr[0] + 1; i > 1; i--)
+    {
+        swap(&heap->arr[1], &heap->arr[i - 1]);
+        heap->arr[0]--;
+        heapify(heap, 1);
     }
-
-
+    heap->arr[0] = lastNdx;
 }
 
 void heapify(HEAP heap, int i)
 {
 
-    int lowNdx = i;
+    int highNdx = i;
     int LC = i * 2;
     int RC = i * 2 + 1;
 
-    if(LC <= heap->arr[0] && heap->arr[LC] < heap->arr[lowNdx]){
-        lowNdx = LC;
+    if (LC <= heap->arr[0] && heap->arr[LC] > heap->arr[highNdx])
+    {
+        highNdx = LC;
     }
 
-    if(RC <= heap->arr[0] && heap->arr[RC] < heap->arr[lowNdx]){
-        lowNdx = RC;
+    if (RC <= heap->arr[0] && heap->arr[RC] > heap->arr[highNdx])
+    {
+        highNdx = RC;
     }
 
-    if(lowNdx != i){
-        swap(&heap->arr[lowNdx], &heap->arr[i]);
-        heapify(heap, lowNdx);
+    if (highNdx != i)
+    {
+        swap(&heap->arr[highNdx], &heap->arr[i]);
+        heapify(heap, highNdx);
     }
-
 }
 
 void swap(int *a, int *b)
