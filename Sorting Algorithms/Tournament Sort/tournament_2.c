3010644bb301279a6d4a0f84a8abcf9b7a96e779
Prince Isaac Pantino <arispantino@gmail.com>
2024-09-26 07:57:51 +0800

    feat(tournament_2): initialized heap and modified helper functions

--- Sorting Algorithms/Tournament Sort/tournament_2.c
+++ Sorting Algorithms/Tournament Sort/tournament_2.c
@@ -6,20 +6,43 @@ void printArray(int arr[], int N);
 void swap(int *a, int *b);
 void tournaSort(int *arr, int size);
 
+typedef struct{
+    int *arr;
+    int lastNdx;
+}*HEAP;
+
 int main(){
 
     int arr[] = { 12, 11, 13, 5, 6, 7, 2, 5, 15 , 1, 0 };
     int N = sizeof(arr) / sizeof(arr[0]);
 
+    HEAP heap = (HEAP)malloc(sizeof(HEAP));
+    if(heap != NULL){
+        heap->arr = (int*)malloc(sizeof(int) * N);
+        heap->lastNdx = -1;
+    }
+
+    int i;
+    for(i = 0; i < N; i++){
+        heap->lastNdx++;
+        heap->arr[heap->lastNdx] = arr[i];
+    }
+
     printf("Initial Array: \n");
-    printArray(arr, N);
+    printArray(heap->arr, heap->lastNdx);
 
     // tournaSort(arr, N);
 
     printf("\n\nSorted Array: \n");
-    printArray(arr, N);
+    printArray(heap->arr, heap->lastNdx);
 
     return 0;
+}
+
+void tournaSort(int *arr, int size){
+
+    
+
 }
 
 void swap(int *a, int *b){
@@ -30,7 +53,7 @@ void swap(int *a, int *b){
 
 void printArray(int arr[], int N)
 {
-    for (int i = 0; i < N; i++){
+    for (int i = 0; i <= N; i++){
         printf("%d ", arr[i]);
     }
 
