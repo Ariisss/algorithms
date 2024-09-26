e8ba8a39cacf707b9f47ad2634141415989749f7
Prince Isaac Pantino <arispantino@gmail.com>
2024-09-26 12:28:15 +0800

    docs(gnome.c): added documentation

--- Sorting Algorithms/Gnome Sort/gnome.c
+++ Sorting Algorithms/Gnome Sort/gnome.c
@@ -7,8 +7,8 @@ void swap(int *a, int *b);
 
 int main(){
 
-    int size = 10;
-    int arr[] = {8, 6, 4, 9, 0, 1, 2, 6, 3, 9};
+    int arr[] = {8, 6, 4, 9, 0, 1, 2, 6, 3, 9, 2, 0, 12, 1, 3};
+    int size = sizeof(arr) / sizeof(arr[0]);
 
     gnomeSort(arr, size);
 
@@ -24,13 +24,18 @@ void gnomeSort(int *arr, int size){
 
     int i = 0;
 
+    // loop until size - 1 (to avoid accessing out of bounds like when i = size - 1 and when we access i + 1 it will be invalid).
     while(i < size - 1){
+
+        // if the current index's value and the next to it is unsorted, we swap then move backwards
+        // P.S: Move only when i > 0
         if(arr[i] > arr[i+1]){
             swap(&arr[i], &arr[i+1]);
             if(i > 0){
                 i--;
             }
         }else{
+            // if it's already sorted, we move forward
             i++;
         }
     }
