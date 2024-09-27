dc150ebc1a8000f7c18bc30ed2f6b690520374ee
Prince Isaac Pantino <arispantino@gmail.com>
2024-09-27 12:41:53 +0800

    feat(quicksort_hoare.c): implemented hoare partitioning scheme/version

--- Sorting Algorithms/Quick Sort/quicksort_hoare.c
+++ Sorting Algorithms/Quick Sort/quicksort_hoare.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define MAX 6
+// #define MAX 10
 
 typedef struct
 {
@@ -13,7 +15,53 @@ void swap(int *x, int *y);
 void printArray(int arr[], int N);
 
 int main(){
+    ARRAY arr;
+    arr.elems = (int *)malloc(sizeof(int) * MAX);
+    arr.count = 0;
 
+    // int values[] = {5, 1, 3, -3, 0, 7};
+    int values[] = {6, 5, 4, 3, 2, 1};
+    // int values[] = {8, 2, 7, -5, 0, 9, 1, 2, -1, 3};
+
+
+    int i;
+    for (i = 0; i < MAX; i++)
+    {
+        arr.elems[i] = values[i];
+        arr.count++;
+    }
+
+    quicksort(&arr, 0, MAX-1);
+    printArray(arr.elems, MAX);
+}
+
+void quicksort(ARRAY *arr, int low, int high){
+
+    if(low < high){
+        int pivot = partition(arr, low, high);
+        quicksort(arr, low, pivot-1);
+        quicksort(arr, pivot+1, high);
+    }
+
+}
+
+int partition(ARRAY *arr, int low, int high){
+
+    int pivot = arr->elems[high];
+    int i = low, j = high;
+
+    while(i < j){
+        
+        while(i < j && arr->elems[i] < pivot) i++;
+        while(j > i && arr->elems[j] >= pivot) j--;
+
+        if(i < j){
+            swap(&arr->elems[j], &arr->elems[i]); 
+        }
+    }
+
+    swap(&arr->elems[i], &arr->elems[high]);
+    return i;
 }
 
 void swap(int *x, int *y){
