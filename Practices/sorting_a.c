6d86fa21a9e635c3de2a0bf854de20c9a4675f71
Prince Isaac Pantino <arispantino@gmail.com>
2024-09-28 14:19:30 +0800

    feat(sorting_a.c): initialized practice file layout

---
+++ Practices/sorting_a.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX 10
+
+// 1) Selection Sort
+void selectionSort(int *arr);
+// 2) Bubble Sort
+void bubbleSort(int *arr);
+// 3) Insertion Sort
+void insertionSort(int *arr);
+// 4) Heapsort Sort
+void heapSort(int *arr);
+// 5) Shell Sort
+void shellSort(int *arr);
+// 6) Comb Sort
+void combSort(int *arr);
+// 7) Tournament Sort
+void tournamentSort(int *arr);
+
+// Util Functions
+void printArray(int arr[], int size);
+void swap(int *a, int *b);
+
+int main(){
+
+    int arr[] = {27, 14, 89, 3, 56, 42, 18, 75, 9, 33};
+
+    
+
+}
+
+void selectionSort(int *arr){
+
+}
+
+void printArray(int arr[], int N)
+{
+    for (int i = 0; i < N; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+}
+
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
