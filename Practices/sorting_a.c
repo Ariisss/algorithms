25ea0e2fe54ef92c7f39cf2d2a8dad462489f5cb
Prince Isaac Pantino <arispantino@gmail.com>
2024-09-29 00:02:38 +0800

    feat(sorting_a): implemented ascending heapsort and comb sort

--- Practices/sorting_a.c
+++ Practices/sorting_a.c
@@ -5,6 +5,7 @@
 
 int SWAPS = 0;
 int ITERATIONS = 0;
+int SHIFTS = 0;
 
 // 1) Selection Sort
 void selectionSort(int *arr);
@@ -12,8 +13,9 @@ void selectionSort(int *arr);
 void bubbleSort(int *arr);
 // 3) Insertion Sort
 void insertionSort(int *arr);
-// 4) Heapsort Sort
+// 4) Heapsort Sort (Ascending)
 void heapSort(int *arr);
+void maxHeapify(int *arr, int max, int ndx);
 // 5) Shell Sort
 void shellSort(int *arr);
 // 6) Comb Sort
@@ -30,22 +32,117 @@ int main()
 
     int arr[] = {27, 14, 89, 3, 56, 42, 18, 75, 9, 33};
 
-    selectionSort(arr);
+    // selectionSort(arr);
     // bubbleSort(arr);
     // insertionSort(arr);
+    // heapSort(arr);
+    // shellSort(arr);
+    combSort(arr);
 
     printArray(arr, MAX);
+}
+
+void combSort(int *arr){
+
+    int i, j, gap;
+    int swapped = 1;
+
+    while(gap != 1 || swapped == 1){
+
+        gap /= 1.3;
+
+        if(gap < 1){
+            gap = 1;
+        }
+
+        swapped = 0;
+
+        for(i = 0; i+gap < MAX; i++){
+            if(arr[i] > arr[i+gap]){
+                swap(&arr[i], &arr[i + gap]);
+                swapped = 1;
+                SWAPS++;
+            }
+            ITERATIONS++;
+        }
+
+    }
+
+}
+
+void shellSort(int *arr){
+
+    int i, j, gap;
+
+    for(gap = MAX/2; gap > 0; gap /= 2){
+
+        for(i = gap; i < MAX; i++){
+
+            int key = arr[i];
+            for(j = i; j >= gap && key < arr[j-gap]; j -= gap){
+                arr[j] = arr[j-gap];
+                ITERATIONS++;
+                SHIFTS++;
+            }
+
+            ITERATIONS++;
+            arr[j] = key;
+        }   
+
+    }
+
+}
+
+void heapSort(int *arr){
+
+    int i;
+    for(i = (MAX-1)/2; i >= 0; i--){
+        maxHeapify(arr, MAX, i);
+    }
+
+    for(i = MAX-1; i >= 0; i--){
+        swap(&arr[0], &arr[i]);
+        maxHeapify(arr, i, 0);
+    }
+
+}
+
+void maxHeapify(int *arr, int max, int ndx)
+{
+
+    int high = ndx;
+    int LC = ndx * 2 + 1;
+    int RC = LC + 1;
+
+    if (LC < max && arr[LC] > arr[high])
+    {
+        high = LC;
+    }
+
+    if (RC < max && arr[RC] > arr[high])
+    {
+        high = RC;
+    }
+
+    if (high != ndx)
+    {
+        swap(&arr[high], &arr[ndx]);
+        maxHeapify(arr, max, high);
+    }
 }
 
 void insertionSort(int *arr)
 {
     int i, j, key;
 
-    for(i = 0; i < MAX; i++){
+    for (i = 0; i < MAX; i++)
+    {
         key = arr[i];
-        for(j = i - 1; j >= 0 && arr[j] > key; j--){
-            arr[j+1] = arr[j];
+        for (j = i - 1; j >= 0 && arr[j] > key; j--)
+        {
+            arr[j + 1] = arr[j];
             ITERATIONS++;
+            SHIFTS++;
         }
         ITERATIONS++;
         arr[j + 1] = key;
@@ -101,6 +198,7 @@ void printArray(int arr[], int N)
 
     printf("# of Iterations: %d\n", ITERATIONS);
     printf("# of Swaps: %d\n", SWAPS);
+    printf("# of Shifts: %d\n", SHIFTS);
 
     for (int i = 0; i < N; i++)
     {
