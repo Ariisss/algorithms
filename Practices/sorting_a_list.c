dced623b9e010094f49c2aea332f40d4fdaf902b
Prince Isaac Pantino <arispantino@gmail.com>
2024-09-29 17:39:39 +0800

    feat(sorting_a_list.c): implemented linked list version of selection sort

--- Practices/sorting_a_list.c
+++ Practices/sorting_a_list.c
@@ -7,17 +7,20 @@ int SWAPS = 0;
 int ITERATIONS = 0;
 int SHIFTS = 0;
 
-typedef struct{
+typedef struct
+{
     char name[20];
     int age;
 } DATA;
 
-typedef struct{
+typedef struct
+{
     int id;
     DATA stud;
 } CELL;
 
-typedef struct list{
+typedef struct list
+{
     CELL cell;
     struct list *link;
 } *LIST, NODE;
@@ -53,36 +56,93 @@ int main()
     LIST list = NULL;
 
     populateList(&list, data);
+    // bubbleSort(&list);
+    selectionSort(&list);
     printList(list);
 
 }
 
-void populateList(LIST *list, CELL arr[]){
+void selectionSort(LIST *L){
+
+    int swapped = 1;
+    LIST *trav = L;
+
+    while(swapped || *trav != NULL){
+
+        LIST *trav2;
+        swapped = 0;
+        // CELL min = (*trav)->cell;
+        LIST *min = trav;
+
+        for( trav2 = trav; *trav2 != NULL; trav2 = &(*trav2)->link ){
+            if((*min)->cell.stud.age > (*trav2)->cell.stud.age){
+                min = trav2;
+                swapped = 1;
+            }
+        }
+
+        if(min != trav){
+            CELL temp = (*min)->cell;
+            (*min)->cell = (*trav)->cell;
+            (*trav)->cell = temp;
+        }
+
+        trav = &(*trav)->link;
+
+    }
+}
+
+void bubbleSort(LIST *L)
+{
+
+    int swapped = 1;
+    LIST *trav = L;
+
+    while (swapped == 1)
+    {
+        swapped = 0;
+        for (trav = L; (*trav)->link != NULL; trav = &(*trav)->link)
+        {
+            if ((*trav)->cell.stud.age > (*trav)->link->cell.stud.age)
+            {
+                CELL temp = (*trav)->cell;
+                (*trav)->cell = (*trav)->link->cell;
+                (*trav)->link->cell = temp;
+                swapped = 1;
+            }
+        }
+    }
+}
+
+
+void populateList(LIST *list, CELL arr[])
+{
 
     LIST *trav = list;
     int i;
 
-    for(i = 0; i < MAX; i++){
-        
+    for (i = 0; i < MAX; i++)
+    {
+
         LIST temp = malloc(sizeof(NODE));
-        if(temp != NULL){
+        if (temp != NULL)
+        {
             temp->cell = arr[i];
             temp->link = NULL;
         }
 
         *trav = temp;
         trav = &(*trav)->link;
-
     }
-
 }
 
-void printList(LIST list){
+void printList(LIST list)
+{
 
-    for(; list != NULL; list = list->link){
+    for (; list != NULL; list = list->link)
+    {
         printf("ID: %-5d | Name: %-15s | Age: %-4d\n", list->cell.id, list->cell.stud.name, list->cell.stud.age);
     }
-
 }
 
 void printArray(int arr[], int N)
