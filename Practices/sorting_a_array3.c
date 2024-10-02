e3798312905e01f2a2801671ec41cb1f22770a6b
Prince Isaac Pantino <arispantino@gmail.com>
2024-10-02 17:37:52 +0800

    feat: implemented selection, bubble, insertion and shell sorts

---
+++ Practices/sorting_a_array3.c
@@ -0,0 +1,159 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX 10
+#define INF 9999
+
+// 1) Selection Sort
+void selectionSort(int *arr);
+// 2) Bubble Sort
+void bubbleSort(int *arr);
+// 3) Insertion Sort
+void insertionSort(int *arr);
+// 4) Heapsort Sort (Ascending)
+void heapSort(int *heap);
+int *heapifyArray(int *arr);
+void maxHeapify(int *arr, int root);
+// 5) Shell Sort
+void shellSort(int *arr);
+// 6) Comb Sort
+void combSort(int *arr);
+// 7) Tournament Sort
+void tournamentSort(int *arr);
+
+// Util Functions
+void printArray(int arr[], int size);
+void swap(int *a, int *b);
+
+int main()
+{
+
+    int arr[] = {27, 14, 89, 3, 56, 42, 18, 75, 9, 33};
+
+    // selectionSort(arr);
+    // bubbleSort(arr);
+    // insertionSort(arr);
+    // shellSort(arr);
+    combSort(arr);
+    // tournamentSort(arr);
+
+    // int *heap = heapifyArray(arr);
+    // heapSort(heap);
+
+    // printArray(heap, MAX+1);
+    printArray(arr, MAX);
+}
+
+void combSort(int *arr){
+
+    int i, gap, swapped = 1;
+
+    gap /= 1.3;
+
+
+    for(gap != 1 || swapped){
+
+        
+
+    }
+
+}
+
+void shellSort(int *arr){
+
+    int i, j, gap, key;
+
+    gap = MAX/2;
+    int swapped = 1;
+
+    while(gap != 1 || swapped){
+
+        swapped = 0;
+
+        for(i = gap; i < MAX; i++){
+            key = arr[i];
+            for(j = i; j >= gap && arr[j-gap] > key; j -= gap){
+                arr[j] = arr[j-gap];
+                swapped = 1;
+            }
+            arr[j] = key;
+        }
+
+        gap /= 2;
+
+    }
+}
+
+void insertionSort(int *arr)
+{
+
+    int i, j, key;
+
+    for (i = 0; i < MAX; i++)
+    {
+        key = arr[i];
+        for (j = i; j >= 0 && arr[j - 1] > key; j--)
+        {
+            arr[j] = arr[j - 1];
+        }
+        arr[j] = key;
+    }
+}
+
+void bubbleSort(int *arr)
+{
+
+    int i, j;
+    for (i = 0; i < MAX; i++)
+    {
+        for (j = 0; j < MAX - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(&arr[j], &arr[j + 1]);
+            }
+        }
+    }
+}
+
+void selectionSort(int *arr)
+{
+
+    int i, j;
+    for (i = 0; i < MAX; i++)
+    {
+        int min = i;
+        for (j = i + 1; j < MAX; j++)
+        {
+            if (arr[j] < arr[min])
+            {
+                min = j;
+            }
+        }
+
+        if (min != i)
+        {
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
+
+void printArray(int arr[], int N)
+{
+
+    for (int i = 0; i < N; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    printf("\n");
+}
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
