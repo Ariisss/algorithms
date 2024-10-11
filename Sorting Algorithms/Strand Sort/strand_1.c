42909ded7e86461f89f28eb93b61a9dfe5e7da14
Prince Isaac Pantino <arispantino@gmail.com>
2024-10-11 11:13:08 +0800

    refactor: improved spacing

--- Sorting Algorithms/Strand Sort/strand_1.c
+++ Sorting Algorithms/Strand Sort/strand_1.c
@@ -10,10 +10,11 @@ typedef struct
 
 void swap(int *x, int *y);
 void printArray(int arr[], int N);
-int* strandSort(ARRAY *arr);
+int *strandSort(ARRAY *arr);
 void mergeStrandOutput(int *output, int *outputSize, int *strand, int strandSize);
 
-int main(){
+int main()
+{
     ARRAY arr;
     arr.elems = (int *)malloc(sizeof(int) * MAX);
     arr.count = 0;
@@ -37,59 +38,72 @@ int main(){
 // 4) merge sublist to output
 // 5) repeat 2 until input array is empty
 
-int* strandSort(ARRAY *arr){
+int *strandSort(ARRAY *arr)
+{
 
-    int* sublist = (int*)malloc(sizeof(int) * arr->count);
-    int* output = (int*)malloc(sizeof(int) * arr->count);
+    int *sublist = (int *)malloc(sizeof(int) * arr->count);
+    int *output = (int *)malloc(sizeof(int) * arr->count);
     int inputSize = arr->count;
     int outputSize = 0;
 
-    while(arr->count > 0){
+    while (arr->count > 0)
+    {
         int sublistSize = 0;
         sublist[sublistSize++] = arr->elems[0];
 
         int i, j = 0;
-        for(i = 1; i < arr->count; i++){
-            if(arr->elems[i] > sublist[sublistSize-1]){
+        for (i = 1; i < arr->count; i++)
+        {
+            if (arr->elems[i] > sublist[sublistSize - 1])
+            {
                 sublist[sublistSize++] = arr->elems[i];
-            }else{
+            }
+            else
+            {
                 arr->elems[j++] = arr->elems[i];
             }
         }
 
         arr->count = j;
         mergeStrandOutput(output, &outputSize, sublist, sublistSize);
-
     }
 
     return output;
 }
 
-void mergeStrandOutput(int *output, int *outputSize, int *strand, int strandSize){
+void mergeStrandOutput(int *output, int *outputSize, int *strand, int strandSize)
+{
 
     int mergedSize = *outputSize + strandSize;
-    int *mergedArray = (int*)malloc(sizeof(int) * mergedSize);
+    int *mergedArray = (int *)malloc(sizeof(int) * mergedSize);
 
     int x, y, z;
     x = y = z = 0;
 
-    while(x < *outputSize && y < strandSize){
-        if(output[x] <= strand[y]){
+    while (x < *outputSize && y < strandSize)
+    {
+        if (output[x] <= strand[y])
+        {
             mergedArray[z++] = output[x++];
-        }else{
+        }
+        else
+        {
             mergedArray[z++] = strand[y++];
         }
     }
 
-    while(x < *outputSize){
+    while (x < *outputSize)
+    {
         mergedArray[z++] = output[x++];
     }
 
-    while(y < strandSize){
+    while (y < strandSize)
+    {
         mergedArray[z++] = strand[y++];
     }
 
-    for(x = 0; x < mergedSize; x++){
+    for (x = 0; x < mergedSize; x++)
+    {
         output[x] = mergedArray[x];
     }
 
@@ -98,15 +112,16 @@ void mergeStrandOutput(int *output, int *outputSize, int *strand, int strandSize
     free(mergedArray);
 }
 
-void swap(int *x, int *y){
-	int temp = *x;
-	*x = *y;
-	*y = temp;
+void swap(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
 }
 
 void printArray(int arr[], int N)
-{	
-	int i;
+{
+    int i;
     for (i = 0; i < N; i++)
     {
         printf("%d ", arr[i]);
